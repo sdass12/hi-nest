2d514f360f6e3d32f822c92c060b67a36db530dc
sdass12 <hc09471@gmail.com>
2021-01-25 18:26:33 +0900

    Version 1.02

---
+++ src/movies/dto/create-movie.dto.ts
@@ -0,0 +1,12 @@
+import {IsOptional, IsString} from 'class-validator';
+
+export class CreateMovieDto {
+
+    @IsString()
+    readonly title: string;
+    @IsString()
+    readonly year: number;
+    @IsOptional()
+    @IsString({ each: true })
+    readonly genres: string[];
+}
